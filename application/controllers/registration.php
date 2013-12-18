bf23b2938338edef3cf90cda2130e503ef40aae2
alexghb <qaz1wsx2>
2013-12-18 09:55:15 +0200

    Registration form

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -18,6 +18,7 @@ class Registration extends CI_Controller {
         $this->form_validation->set_rules('username', 'Username', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required');
         $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('tc', 'T&C', 'required');
 
         $this->load->view('templates/header', $data);
         if ($this->form_validation->run() === FALSE)
