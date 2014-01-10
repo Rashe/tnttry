d38e61cf37f287f1beca02c7de9ebfe690306eef
Pumych <pumych@gmail.com>
2014-01-10 16:14:43 +0200

    Login on top header

--- application/views/templates/login.php
+++ application/views/templates/login.php
@@ -2,22 +2,19 @@
 echo '<div id="login_block">' . validation_errors() . '</div>' .
 
     form_open('login') .
-//    form_fieldset() .
-//    form_label('Email', 'email') .
+
+    anchor(base_url().'index.php/registration', 'Хочешь голых сисек маленький проказник? :)', array('class' => 'regLink', 'placeholder' => 'Хочешь голых сисек маленький проказник? :)')).
+
     form_input(array(
         'name' => 'email',
         'type' => 'email',
         'placeholder' => 'Email'
     )) .
-//    form_fieldset_close() .
 
-//    form_fieldset() .
-//    form_label('Password', 'password') .
     form_input(array(
         'name' => 'password',
         'placeholder' => 'Password'
     )) .
-//    form_fieldset_close() .
 
     form_submit('submit', 'Login') .
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     console.log('Main javascript OK');
+
+    $('.loginWrapper form a.regLink').hover( function(){
+        $(this).text('Да хочу! Отрегестрируй меня полностью.')
+    }, function(){
+        $(this).text($(this).attr('placeholder'));
+    });
 });
