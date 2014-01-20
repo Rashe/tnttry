618c6df12cbdb1c720dcb2ea1fde5313ab677e87
raphaelsh <shemeshra@gmail.com>
2014-01-20 18:08:50 +0200

    Static html template added

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,6 @@
     <title><?php echo $title ?> - Our cool site</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/reset.css'); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/header.css'); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/registration.css'); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/style.css'); ?>">
     <script type="text/javascript" src="<?php echo base_url('scripts/jquery-1.10.2.min.js'); ?>"></script>
