7c8e848b95737baff77ac3c9c295582413b681ec
alexghb <alexghb3@gmail.com>
2014-01-22 17:42:37 +0200

    Registration & login

--- application/libraries/login.php
+++ application/libraries/login.php
@@ -1,54 +1,48 @@
 <?php
 class Login {
 
+    private $CI;
+
     public function __construct()
     {
-        $CI =& get_instance();
+        $this->CI =& get_instance();
 
-        $CI->load->helper('url');
-        $CI->load->helper('form');
-        $CI->load->model('usersession_model');
+        $this->CI->load->helper(array('url', 'form'));
+        $this->CI->load->model('usersession_model');
     }
 
     public function index()
     {
-        $CI =& get_instance();
-
-        if(($CI->session->userdata('user_name') != ''))
+        if(($this->CI->session->userdata('user_name') != ''))
         {
-            $data['username'] = $CI->session->userdata('user_name');
+            $data['username'] = $this->CI->session->userdata('user_name');
             return $this->getUserpanel($data, true);
         }
 
-        $CI->load->helper('form');
-        $CI->load->library('form_validation');
+        $this->CI->load->library('form_validation');
 
-        $CI->form_validation->set_rules('email', 'Email', 'required');
-        $CI->form_validation->set_rules('password', 'Password', 'required');
+        $this->CI->form_validation->set_rules('email', 'Email', 'required');
+        $this->CI->form_validation->set_rules('password', 'Password', 'required');
 
-        $email = $CI->input->post('email');
-        $password = $CI->input->post('password');
+        $email = $this->CI->input->post('email');
+        $password = $this->CI->input->post('password');
 
         return $this->login($email, $password);
     }
 
     public function login($email, $password)
     {
-        $CI =& get_instance();
-
-        if(!$CI->usersession_model->login($email, $password)) {
+        if(!$this->CI->usersession_model->login($email, $password)) {
             return $this->getUserpanel(null, false);
         }
 
-        $data['username'] = $CI->session->userdata('user_name');
+        $data['username'] = $this->CI->session->userdata('user_name');
         return $this->getUserpanel($data, true);
     }
 
     private function getUserpanel($data = array(), $loggedIn)
     {
-        $CI =& get_instance();
-
         $data['loggedIn'] = $loggedIn;
-        return $CI->load->view('templates/userpanel', $data, true);
+        return $this->CI->load->view('templates/userpanel', $data, true);
     }
 }
