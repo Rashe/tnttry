5c91ec68723854ea742a5e888ae71a1716e44ccf
raphaelsh <shemeshra@gmail.com>
2014-03-10 14:30:36 +0200

    DataBase updated

--- database/tnttry.sql
+++ database/tnttry.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6deb1
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 23, 2013 at 05:08 PM
--- Server version: 5.5.34-0ubuntu0.13.10.1
--- PHP Version: 5.5.3-1ubuntu2
+-- Generation Time: Mar 10, 2014 at 12:28 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.3.10-1ubuntu3.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -23,6 +23,65 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `coment_like`
+--
+
+CREATE TABLE IF NOT EXISTS `coment_like` (
+  `comment_likeid` int(20) NOT NULL AUTO_INCREMENT,
+  `comment_id` int(20) NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `comment_likedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`comment_likeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment_id` int(20) NOT NULL AUTO_INCREMENT,
+  `comment_body` varchar(512) CHARACTER SET utf8 DEFAULT NULL,
+  `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(30) CHARACTER SET utf8 DEFAULT NULL,
+  `post_id` int(20) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `post_id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_views` int(11) NOT NULL DEFAULT '0',
+  `post_body` varchar(512) NOT NULL,
+  `post_likes` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_likes`
+--
+
+CREATE TABLE IF NOT EXISTS `post_likes` (
+  `post_likeid` int(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `post_likedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` int(20) NOT NULL,
+  PRIMARY KEY (`post_likeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,14 +90,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(30) NOT NULL,
   `password` char(64) NOT NULL,
   `email` varchar(70) NOT NULL,
-  `salt` char(128) NOT NULL,
-  `reg_date` datetime NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_login` datetime DEFAULT NULL,
   `last_ip` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `reg_date`, `last_login`, `last_ip`) VALUES
+(1, 'Lejeboka', '', 'Lejeboka@try.com', '2014-01-01 03:00:00', '2014-01-01 03:00:00', '19.25.36.45'),
+(2, 'Gutelot', '', 'Gutelot@vah.com', '2014-01-02 07:35:00', '2014-01-03 06:37:11', '234.345.56.76');
 
 -- --------------------------------------------------------
 
